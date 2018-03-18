f256f4fa79452c931de607acc92d45982a6516bf
Vinícius Batista Barreira <viniciustes@gmail.com>
2018-03-18 19:23:14 -0300

    Layout pagina de login

---
+++ Viniciustes/Controllers/AccountController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Viniciustes.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        public IActionResult Login1()
+        {
+            return View();
+        }
+
+        public IActionResult Login2()
+        {
+            return View();
+        }
+    }
+}
